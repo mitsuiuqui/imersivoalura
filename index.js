9438cea89529a05b4169336d11f6c8be57128fc7
mitsuiuqui <pimimurillo@gmail.com>
2025-04-02 00:07:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+document.getElementById("startButton").addEventListener("click", function() {
+    comecar();
+});
+
+alert("Voce esta numa prova do ENEM, e precisa chutar o resultado de uma questao!")
+
+function comecar() {
+idade = prompt("quantos anos você tem?")
+if(idade >= 16) {
+    alert("você pode fazer a prova do ENEM")
+}
+// alert("vamos começar a jogar!")
+if(idade >= 16) {
+    let escolhaJogador = prompt ("A, B, C ou D?")
+    let escolhaComputador = Math.floor(Math.random()* 4) + 1;
+
+    if(escolhaComputador == 1) {
+        escolhaComputador = "A"
+    }
+    if(escolhaComputador == 2) {
+        escolhaComputador = "B"
+    }
+    if(escolhaComputador == 3) {
+        escolhaComputador = "C"
+    }
+    if(escolhaComputador == 4) {
+        escolhaComputador = "D"
+    }
+
+    if(escolhaJogador == escolhaComputador) {
+        alert("Voce acertou!!")
+    } else {
+        alert(`Voce errou, a resposta era ${escolhaComputador}`)
+    }
+
+    if(escolhaComputador == 1) {
+        escolhaComputador = "A"
+    }
+    if(escolhaComputador == 2) {
+        escolhaComputador = "B"
+    }
+    if(escolhaComputador == 3) {
+        escolhaComputador = "C"
+    }
+    if(escolhaComputador == 4) {
+        escolhaComputador = "D"
+    }
+}
+}
