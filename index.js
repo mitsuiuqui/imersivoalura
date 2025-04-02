71be734b0007ae56c0495009ca1e9b1fe948a90c
mitsuiuqui <pimimurillo@gmail.com>
2025-04-02 00:28:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -32,18 +32,34 @@ if(idade >= 16) {
     } else {
         alert(`Voce errou, a resposta era ${escolhaComputador}`)
     }
+    let jogarNovamente = true;
 
-    if(escolhaComputador == 1) {
-        escolhaComputador = "A"
+    while (jogarNovamente) {
+        let escolhaJogador = prompt("A, B, C ou D?").toUpperCase();
+        let escolhaComputador = Math.floor(Math.random() * 4) + 1;
+    
+        if (escolhaComputador == 1) {
+            escolhaComputador = "A";
+        } else if (escolhaComputador == 2) {
+            escolhaComputador = "B";
+        } else if (escolhaComputador == 3) {
+            escolhaComputador = "C";
+        } else if (escolhaComputador == 4) {
+            escolhaComputador = "D";
+        }
+    
+        if(escolhaJogador == escolhaComputador) {
+            alert("Voce acertou!!")
+        } else {
+            alert(`Voce errou, a resposta era ${escolhaComputador}`)
+        }
+    
+        let novamente = prompt("Deseja jogar novamente? (S/N)").toUpperCase();
+        if (novamente !== "S") {
+            jogarNovamente = false;
+        }
     }
-    if(escolhaComputador == 2) {
-        escolhaComputador = "B"
-    }
-    if(escolhaComputador == 3) {
-        escolhaComputador = "C"
-    }
-    if(escolhaComputador == 4) {
-        escolhaComputador = "D"
-    }
+    
+    console.log("Jogo encerrado!");
 }
 }
