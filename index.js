b33c2e2635c7c651cb0743a7012b477664a32bcc
mitsuiuqui <pimimurillo@gmail.com>
2025-04-02 00:20:23 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(idade >= 16) {
 }
 // alert("vamos começar a jogar!")
 if(idade >= 16) {
-    let escolhaJogador = prompt ("A, B, C ou D? *em maiusculas*")
+    let escolhaJogador = prompt ("A, B, C ou D?").toUpperCase()
     let escolhaComputador = Math.floor(Math.random()* 4) + 1;
 
     if(escolhaComputador == 1) {
